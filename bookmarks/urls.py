70258bd0caae7d1052c9839033e83f2cb1bdedb5
Naveen <nbanala@unomaha.edu>
2017-11-10 00:10:39 -0600

    urls code updated

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^account/', include('account.urls')),
+    url(r'', include('account.urls')),
 ]
 
 if settings.DEBUG:
